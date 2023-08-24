6e8bf1dbecdac96427375841da72e2940e019724
Cakesniffer-Ben <benjamward12@gmail.com>
2023-08-24 13:39:59 +0100

    Initial commit

---
+++ src/FibMain.java
@@ -0,0 +1,17 @@
+import java.math.BigInteger;
+
+public class FibMain {
+    public static void main(String[] args) {
+        BigInteger a = BigInteger.valueOf(0);
+        BigInteger b = BigInteger.valueOf(1);
+        fibRecursive(a, b, 100);
+    }
+
+    public static void fibRecursive(BigInteger nMinus1, BigInteger nMinus2, int sequenceLength){
+        BigInteger n = nMinus1.add(nMinus2);
+        System.out.println(n);
+        if (sequenceLength > 0) {
+            fibRecursive(n, nMinus1, sequenceLength-1);
+        }
+    }
+}
