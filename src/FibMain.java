13c4733beb2f5be97f55b85b2811031150948777
Cakesniffer-Ben <benjamward12@gmail.com>
2023-08-25 11:39:20 +0100

    Fixed bug where additional value was printed beyond the specified range.

--- src/FibMain.java
+++ src/FibMain.java
@@ -10,7 +10,7 @@ public class FibMain {
     public static void fibRecursive(BigInteger nMinus1, BigInteger nMinus2, int sequenceLength){
         BigInteger n = nMinus1.add(nMinus2);
         System.out.println(n);
-        if (sequenceLength > 0) {
+        if (sequenceLength > 1) {
             fibRecursive(n, nMinus1, sequenceLength-1);
         }
     }
